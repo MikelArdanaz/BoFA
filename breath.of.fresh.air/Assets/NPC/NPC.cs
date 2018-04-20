c5c3e4d25cd2e7a935deea15c2f7b68e817ba91c
Richard <rbanyi@me.com>
2018-04-20 16:41:39 +0800

    function for determining location of NPCs

---
+++ breath.of.fresh.air/Assets/NPC/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+	float _health = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void DecreaseLife(float damage){
+		_health -= damage;
+		if (_health <= 0f) {
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ breath.of.fresh.air/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float panSpeed = 30f; 
+	public Vector2 panLimit; 
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if(Input.GetKeyUp("w")) 
+		{
+			pos.z += panSpeed;
+		}
+		if(Input.GetKeyUp("a")) 
+		{
+			pos.x -= panSpeed;
+		}
+		if(Input.GetKeyUp("s")) {
+
+			pos.z -= panSpeed;
+		}
+		if(Input.GetKeyUp("d")) 
+		{
+			pos.x += panSpeed;
+		}
+
+//		if(Input.GetKey("w")) 
+//		{
+//			pos.z += panSpeed + Time.deltaTime;
+//		}
+//		if(Input.GetKey("a")) 
+//		{
+//			pos.x -= panSpeed + Time.deltaTime;
+//		}
+//		if(Input.GetKey("s")) {
+//
+//			pos.z -= panSpeed + Time.deltaTime;
+//		}
+//		if(Input.GetKey("d")) 
+//		{
+//			pos.x += panSpeed + Time.deltaTime;
+//		}
+		pos.x = Mathf.Clamp (pos.x, -panLimit.x, panLimit.x); 
+		pos.z = Mathf.Clamp (pos.z, -panLimit.y, panLimit.y);
+
+		transform.position = pos;
+	}
+}
